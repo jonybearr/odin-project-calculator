4ed285c5ddfdb4aba365eeaea50ee3869857f8df
jonybearr <65643049+jonybearr@users.noreply.github.com>
2023-04-07 00:01:06 +0800

    linked button presses to display refreshes
    as well as clr button clears display

--- backend.js
+++ backend.js
@@ -4,7 +4,7 @@ console.log('hello there')
 
 //operator functions
 
-let displayedString='Display';
+let displayedString='';
 
 function add(a,b) {
     return a+b;
@@ -21,8 +21,25 @@ function divide(a,b) {
 
 //reaction functions
 
+//setup reactions
 let buttons = document.querySelectorAll('.grid-item');
 
 buttons.forEach(button => {
-    button.addEventListener('click', function(e){console.log(e.target.id)});
+    button.addEventListener('click', function(e){displayUpdate(e)});
 });
+
+//reaction for display
+function displayUpdate(event) {
+    let newThing=event.target.id
+    console.log(newThing);
+
+    //if clear button is pressed, clear the display
+    if(newThing=="clear") {
+        displayedString="";
+    } else {
+        displayedString=displayedString+newThing;
+    }
+
+    //connect to html
+    document.querySelector('#display').innerHTML=displayedString;
+}
