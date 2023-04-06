37cb25ac7dc705ba824a7e467a42c1bcee008684
jonybearr <65643049+jonybearr@users.noreply.github.com>
2023-04-06 23:28:55 +0800

    linked html buttons with js
    enabled console returns based on button clicks

--- backend.js
+++ backend.js
@@ -1,3 +1,28 @@
 
 
 console.log('hello there')
+
+//operator functions
+
+let displayedString='Display';
+
+function add(a,b) {
+    return a+b;
+}
+function subtract(a,b) {
+    return a-b;
+}
+function multiply(a,b) {
+    return a*b;
+}
+function divide(a,b) {
+    return a/b;
+}
+
+//reaction functions
+
+let buttons = document.querySelectorAll('.grid-item');
+
+buttons.forEach(button => {
+    button.addEventListener('click', function(e){console.log(e.target.id)});
+});
