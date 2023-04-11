ede9b98f1e7b0f7755ebaad32e68d7fe2ea09273
jonybearr <65643049+jonybearr@users.noreply.github.com>
2023-04-11 22:51:07 +0800

    established functionality for basic calculations
    equals button logic works and updates display accordingly

--- backend.js
+++ backend.js
@@ -19,27 +19,87 @@ function divide(a,b) {
     return a/b;
 }
 
-//reaction functions
+function operate(operator,a,b) {
+    if(operator=='*') {
+        return multiply(a,b);
+    }
+    if(operator=='/') {
+        return divide(a,b);
+    }
+    if(operator=='+') {
+        return add(a,b);
+    }
+    if(operator=='-') {
+        return subtract(a,b);
+    }
+}
 
-//setup reactions
+
+
+//logic functions
+
+//reaction setup reactions
 let buttons = document.querySelectorAll('.grid-item');
 
 buttons.forEach(button => {
     button.addEventListener('click', function(e){displayUpdate(e)});
 });
 
+
+//establish variables
+let operation = '';
+let value1 = '';
+let value2 = '';
+let answer='';
+
+
 //reaction for display
 function displayUpdate(event) {
     let newThing=event.target.id
     console.log(newThing);
 
-    //if clear button is pressed, clear the display
+    //button pressed logic
+    //if clear button is pressed, clear the display, clear variables
     if(newThing=="clear") {
         displayedString="";
+        operation = '';
+        value1 = '';
+        value2 = '';
+    //if operator button is pressed, store in variable
+    } else if(newThing=='+'||newThing=='-'||newThing=='*'||newThing=='/') {
+        //update variable
+        operation = newThing;
+        console.log('new operator is ' +newThing)
+
+    //if evaluate button is pressed, 
+    } else if(newThing=='=') {
+        console.log('calculation ran!')
+        //send values to operate function
+        answer=operate(operation,value1,value2);
+        //update display with answer
+        displayedString=answer;
+        //update variables to match logic
+        
+    //if operator exists, any new numbers are for the second value
+    } else if(operation!='') {
+        //if the value2 is empty, 
+        if(value2=='') {
+            //store previous number in value 1
+            value1=displayedString;
+            //clear screen of previous number, and update new number
+            displayedString=newThing;
+            //update value2
+            value2=newThing;
+        } else {
+            displayedString=displayedString+newThing;
+            value2=displayedString;
+        }
+            
+    
     } else {
         displayedString=displayedString+newThing;
     }
 
     //connect to html
-    document.querySelector('#display').innerHTML=displayedString;
+    document.querySelector('#display').textContent=displayedString;
 }
