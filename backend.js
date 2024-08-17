49c05c2c73e6d072ddb0e75309d438a910306772
jonybearr <jonybear@duck.com>
2024-08-17 23:09:34 +0800

    feat: add del function, fix repeat operations and other edge cases

--- backend.js
+++ backend.js
@@ -1,11 +1,13 @@
+//establish variables
+let operation = '';
+let value1 = '';
+let value2 = '';
+let answer='';
+let displayedString='';
+let prevEventWasEquals
 
-
-console.log('hello there')
 
 //operator functions
-
-let displayedString='';
-
 function add(a,b) {
     return a+b;
 }
@@ -19,7 +21,10 @@ function divide(a,b) {
     return a/b;
 }
 
+
 function operate(operator,a,b) {
+    a=Number(a)
+    b=Number(b)
     if(operator=='*') {
         return multiply(a,b);
     }
@@ -46,70 +51,94 @@ buttons.forEach(button => {
 });
 
 
-//establish variables
-let operation = '';
-let value1 = '';
-let value2 = '';
-let answer='';
-
-
 //reaction for display
 function displayUpdate(event) {
-    let newThing=event.target.id
-    console.log(newThing);
+    let newAction=event.target.id
+    console.log(newAction);
 
     //button pressed logic
+
+
     //if clear button is pressed, clear the display, clear variables
-    if(newThing=="clear") {
-        displayedString="0";
+    if(newAction=="clear") {
+        displayedString="";
         operation = '';
         value1 = '';
         value2 = '';
-        answer='';
+
     //if operator button is pressed
-    } else if(newThing=='+'||newThing=='-'||newThing=='*'||newThing=='/') {
-        //check if value1 exists
-        if(value1==''){
-            value1=answer;
+    } else if(newAction=='+'||newAction=='-'||newAction=='*'||newAction=='/') {
+        //if existing operations are pending, do the operations and set the result as
+        //the first value of the new operation
+        if(value1!=''&&value2!=''&&operation!=''){
+            answer = operate(operation,value1,value2)
+            displayedString=answer
+            value1=answer
+            operation=newAction
+            //reset value2
+            value2=''
+        } else if (value1 !=''){
+            operation=newAction
+            displayedString=operation
+        } else {
+            //do nothing 
         }
-        //update variable
-        operation = newThing;
-        console.log('new operator is ' +newThing)
 
     //if evaluate button is pressed, 
-    } else if(newThing=='=') {
-        console.log('calculation ran!')
-        //send values to operate function
-        answer=operate(operation,value1,value2);
-        //update display with answer
-        displayedString=answer;
-        //update variables to match logic
-        //clear all variables except answer
-        operation = '';
-        value1 = '';
-        value2 = '';
-        
-    //if operator exists, any new numbers are for the second value
-    } else if(operation!='') {
-        //clear screen of previous number, and update new number
-        displayedString=newThing;
-        //update value2
-        value2=newThing;
-
-            
-    //first value
-    } else {
-        //check if it is the first digit of the first value
-        if(value1==''){
-            displayedString=newThing;
-            value1=newThing;
+    } else if(newAction=='=') {
+        //send values to operate function if requirements are met
+        if(value1!=''&&value2!=''&&operation!=''){
+            console.log('calculation running!')
+            answer=operate(operation,Number(value1),Number(value2));
+            //update display with answer
+            displayedString=answer;
+            value1 = answer;
+            console.log(answer)
+            //delete old variables
+            operation = '';
+            value2 = '';
+            prevEventWasEquals=true
+            console.log(prevEventWasEquals)
         } else {
-            displayedString=displayedString+newThing;
-            value1=displayedString;
+            console.log('missing inputs')
+        }
+    //del button    
+    } else if(newAction=='del') {
+        //edit value 1 if operation doesnt exist
+        if(operation==''){
+            //if single digit, del clears value1 and sets display to empty
+            if(value1.length==1){
+                value1=''
+                displayedString=''
+            } else {
+                value1=value1.substring(0,value1.length-1)
+                displayedString=value1
+            }
+        } else {
+            value2=value2+newAction;
+            displayedString=value2;
+        }
+             
+    //if a number is pressed
+    } else {
+        //if operator is not pressed, add to first number
+        if(operation==''){
+            if(prevEventWasEquals==true){
+                prevEventWasEquals=false
+                value1=''
+                console.log('this is value1 right now ' + value1)
+            }
+            value1=value1+newAction;
+            displayedString = value1;
+
+        //else add to second number
+        } else {
+            value2=value2+newAction;
+            displayedString=value2;
         }
         
     }
 
-    //connect to html
+    //write displayed string to html
     document.querySelector('#display').textContent=displayedString;
 }
